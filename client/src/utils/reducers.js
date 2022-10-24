ad367e6d572397f02aed20f7aee6a55acc8c9ce2
Codeyg12 <codey.gallup@gmail.com>
2022-10-24 11:46:32 -0600

    updated store, minor changes to reducers and app

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -19,7 +19,7 @@ const initialState = {
   currentCategory: '',
 }
 
-export const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_PRODUCTS:
       return {
@@ -92,6 +92,8 @@ export const reducer = (state = initialState, action) => {
   }
 };
 
-export function useProductReducer(initialState) {
-  return useReducer(reducer, initialState);
-}
+// export function useProductReducer(initialState) {
+//   return useReducer(reducer, initialState);
+// }
+
+export default reducer
--- client/src/utils/store.js
+++ client/src/utils/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import reducers from "./reducers";
+import reducer from "./reducers";
 
-export default configureStore(reducers);
+export default configureStore({ reducer: reducer });
